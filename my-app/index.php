f3f1c7b725cf5b4b5079aa978c5fa4e7dd1906ae
Vito Gentile <gentilevito1076@gmail.com>
2021-03-02 08:10:49 +0100

    File Preparati e Collegati

---
+++ my-app/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP-Google-Faq</title>
+  </head>
+  <body>
+
+    <!-- Esercizio di oggi Google Faq
+    repo/cartella: php-google-faq
+    Riscrivere questa pagina del sito google
+    https://policies.google.com/faq.
+    Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+    Vi consiglio di approcciare l'esercizio prima solo lato PHP, una volta che array + echo delle domande e riposte funziona, potete quindi lavorare in SCSS per ricreare la grafica della pagina.
+       -->
+
+    <!-- HEADER -->
+    <header>
+
+    </header>
+    <!-- END HEADER -->
+
+    <!-- MAIN -->
+    <main>
+      prova
+    </main>
+    <!-- END MAIN -->
+
+
+  </body>
+</html>
